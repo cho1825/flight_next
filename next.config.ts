bc8fee670eece632cd2be3207255b96d01362614
cho1825 <devcho1825@gmail.com>
2025-07-14 11:08:28 +0900

    fix - 사이트맵, 로봇.ts 수정 변경

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    async headers() {
+        return [
+            {
+                source: "/sitemap.xml",
+                headers: [
+                    {
+                        key: "Content-Type",
+                        value: "application/xml",
+                    },
+                ],
+            },
+            {
+                source: "/sitemap-:index.xml",
+                headers: [
+                    {
+                        key: "Content-Type",
+                        value: "application/xml",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
